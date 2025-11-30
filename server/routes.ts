e9eba5874ea08918a4bb6ab3421be9b230e3c790
Nkinnas <124389633+Nkinnas@users.noreply.github.com>
2025-11-29 18:29:32 -0600

    fix login redirect issue

--- server/routes.ts
+++ server/routes.ts
@@ -36,7 +36,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       },
     })
